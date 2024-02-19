d8232c098589bf1238e812f5f71d335d366d6e58
Philipp Neuhaus <philipp.neuhaus@uni-muenster.de>
2024-02-19 20:40:11 +0100

    Added a job limitation. Sorry, no issue tracker online, so a PR is the only option ;-)

--- local-build.sh
+++ local-build.sh
@@ -3,6 +3,11 @@
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-BRANCH=test
+BRANCH=main
 
-powerprofilesctl launch flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build com.bambulab.BambuStudio.yml --repo=_repo
+# I do have 32 GB of RAM 
+# and 16 Cores. But compiling without a job limitation
+# always gave me a "Killed with signal 15".
+# Limiting to number of cores with a max of 4GB per job per script may help, too.
+
+powerprofilesctl launch flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build io.github.softfever.OrcaSlicer.yml --repo=_repo --jobs=4
